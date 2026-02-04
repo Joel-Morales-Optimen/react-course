3d0ab3b927df36bed8134a008abc9837d7bb668f
Joel-Morales-Optimen <joel.morales@optimen.com.mx>
2026-02-04 17:20:03 -0600

    Avances 2.39.27

--- src/index.js
+++ src/index.js
@@ -4,33 +4,9 @@ import { Posts } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const users = [
-  {
-    id: 1,
-    name: "Ryan Ray",
-    image: "https://robohash.org/usuario1",
-  },
-  {
-    id: 2,
-    name: "Joe",
-    image: "https://robohash.org/usuario2",
-  },
-  {
-    id: 2,
-    name: "Luis",
-    image: "https://robohash.org/usuario3",
-  },
-];
 
 root.render(
   <>
-    {users.map((user, i) => {
-      return (
-        <div key={i}>
-          <h1>{user.name}</h1>
-          <img src={user.image} />
-        </div>
-      );
-    })}
+    <h1>Counter: 0</h1>
   </>,
 );
