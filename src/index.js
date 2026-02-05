e88718925465118dc3aeec80b042e3579e768c7f
FOX271987 <luisjoeleon536@gmail.com>
2026-02-04 23:28:57 -0600

    Ya

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { Posts } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Counter() {
+  const [mensaje, setMensaje] = useState("");
+  const [ counter, setCounter ] = useState(0);
 
-  const [counter, setCounter] = useState(10)
+  useEffect(() => {
+    console.log("render");
+  }, [counter]);
 
   return (
     <div>
-      <h1>Counter: {counter}</h1>
+      <input onChange={(e) => setMensaje(e.target.value)} />
       <button
         onClick={() => {
-          setCounter(100)
+          alert("El mensaje es: " + mensaje);
         }}
       >
-        Sumar
+        Save
+      </button>
+      <hr/>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>
+        Incrementar
       </button>
     </div>
   );
 }
 
-root.render(<>
-  <Counter />
-</>);
+root.render(
+  <>
+    <Counter />
+  </>,
+);
